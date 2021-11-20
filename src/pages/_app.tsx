93113f5f562dc2bc44797e3169f86102846aa0fa
Italo Magno <italomagno10@gmail.com>
2021-11-20 17:47:04 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from "next/app"
+
+import '../syles/global.scss'
+
+function MyApp({ Component, pageProps } : AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+
+import  Head  from 'next/head'
+
+export default function Home() {
+  return (  
+    <>
+    <Head>
+      <title>Início | Ignews</title>
+    </Head>
+
+    <h1> 
+
+
+    Hello <span>Italo</span> 
+    </h1>
+    </>
+  )
+}
