bc422d33a334becc093827130aee7b34951f3eb8
Italo Magno <italomagno10@gmail.com>
2021-11-22 11:05:58 -0300

    last commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 
+import { GetStaticProps } from 'next'
 import  Head  from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 import styles from './home.module.scss'
 
-export default function Home() {
+/*========3 FORMAS DE FAZER UMA CHAMADA API =========*/
+// Client-side ------ UseEffect()
+// Server-side ------ GetServerProps
+// Static-side ------ GetStaticProps
+
+
+interface HomeProps{
+  product: {
+    priceId: string,
+    amount: Number
+  }
+}
+
+export default function Home( { product } : HomeProps) {
+  console.log(product)
+
   return (  
     <>
     <Head>
@@ -18,10 +35,10 @@ export default function Home() {
         <h1>News about the <span>React</span> world.</h1>
         <p>
           Get access to all the publications <br />
-          <span>for $9.98 month</span>
+          <span>for {product.amount} month</span>
         </p>
 
-        <SubscribeButton />
+        <SubscribeButton priceId = {product.priceId}/>
 
       </section>
 
@@ -29,4 +46,27 @@ export default function Home() {
     </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JyRgBGWz18WwxxgqGybsANS', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+
+  }
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 , // 24 hours
+
+  }
+
 }
