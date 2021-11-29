1cdb3c632d0f0ed1f0d333a75b6f40bd1c1a7239
Italo Magno <italomagno10@gmail.com>
2021-11-29 15:40:39 -0300

    finishonemorestep

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+import Link, { LinkProps } from "next/link";
+import { ReactElement, cloneElement } from "react";
+
+
+interface ActiveLinkProps extends LinkProps{
+  children: ReactElement;
+  ActiveClassName: string;
+}
+
+export function ActiveLink({children, ActiveClassName, ...rest}: ActiveLinkProps){
+
+  const {asPath} = useRouter()
+
+  const className = asPath === rest.href
+  ? ActiveClassName
+  : '';
+
+  return(
+    <Link {...rest}>
+      {cloneElement(children, {
+        className,
+      }) }
+    </Link>
+  )
+}
